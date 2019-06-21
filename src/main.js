c261a1cd5d6e4d0508bb4e099a05a3ae9783a705
UniqueCmn <2757997493@qq.com>
2019-06-21 09:52:29 +0800

    清空默认，调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+// 程序启动
+
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './App.vue' // 加载根组件
+
+// 优先查找文件，文件找不到就找目录
+// 找到目录，优先加载目录中的index.js
+import router from './router' // 加载路由
 
 Vue.config.productionTip = false
 
+// 创建vue实例
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App) // 将根组件替换到页面节点
 }).$mount('#app')
